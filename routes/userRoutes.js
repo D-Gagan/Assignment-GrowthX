55cbcd710dfb20d61bda39e88424721183aef0d5
Gagan D <76513704+D-Gagan@users.noreply.github.com>
2024-10-11 11:25:31 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
-import { register, fetchUsers, updateUser, deleteUser } from "../controller/userController.js";
-
+import { register, login } from "../controller/userController.js";
 const route = express.Router();
 
 // User Routes
 route.post("/register", register);
-route.get("/getallusers", fetchUsers);
-route.put("/update/:id", updateUser);
-route.delete("/delete/:id", deleteUser);
+route.post("/login", login);
 
 export default route;
